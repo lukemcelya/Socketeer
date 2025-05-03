e4ba5b451d822ba16ed4dae8c06bc2a2ce097236
lukemcelya <johnlukemcelya@gmail.com>
2025-05-03 16:37:13 -0500

    Everything is working :)

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,10 +5,16 @@ from .models import Room, Message
 
 connected_clients = {}
 
+@database_sync_to_async
+def save_message(user, room_name, text):
+    room = Room.objects.get(name=room_name)
+    return Message.objects.create(room=room, user=user, text=text)
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        self.username = self.scope["user"].username
 
         # Join room group
         await self.channel_layer.group_add(
@@ -18,6 +24,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Updating new user join
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'user_join',
+                'username': self.username
+            }
+        )
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
@@ -25,12 +40,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        # Updating user leaving
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'user_leave',
+                'username': self.username
+            }
+        )
+
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
         username = self.scope["user"].username
 
-        await self.save_message(self.scope["user"], self.room_name, message)
+        await save_message(self.scope["user"], self.room_name, message)
 
         # Broadcast to group
         await self.channel_layer.group_send(
@@ -42,13 +66,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    @database_sync_to_async
-    def save_message(user, room_name, text):
-        room = Room.objects.get(name=room_name)
-        return Message.objects.create(room=room, user=user, text=text)
-
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
             "message": event["message"],
             "username": event["username"]
+        }))
+
+    async def user_join(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'user_join',
+            'username': event['username']
+        }))
+
+    async def user_leave(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'user_leave',
+            'username': event['username']
         }))
